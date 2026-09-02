c566eb9f2994d2ffd854c258d487937eaa33cba8
vrdtska <santilopezcer@gmail.com>
2026-09-01 18:21:41 -0600

    aaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://ubiquitous.udem.edu', 
   base: '/~iac-612956',
-  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
   },
